123971d43ef267887913ec7313efc81718f1c107
kaku <1092804319@qq.com>
2026-06-02 01:49:59 +0800

    feat: 更新友链和个人站点信息
    将友链改为 TouchAI，更新站点名称为卡库，补充个人信息。

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -23,12 +23,12 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
-		title: "夏夜流萤",
+		title: "TouchAI",
 		imgurl:
-			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-		siteurl: "https://blog.cuteleaf.cn",
-		tags: ["Blog"],
+			"https://avatars.githubusercontent.com/u/266058215?s=200&v=4",
+		desc: "一触即达的极简桌面效率Agent。",
+		siteurl: "https://github.com/TouchAI-org/TouchAI",
+		tags: ["Agent"],
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
